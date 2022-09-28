a27f061c1ede74c5273b65c05d89057f82d04edf
Maximilian Rohleder <Maximilian.Rohleder.ext@siemens-healthineers.com>
2022-09-28 12:54:26 +0200

    Add separate bind file and header
    - binding is only used with setuptools
    - pure cpp builds fail with pybind11
    - binding is now done in separate file

---
+++ cuda/image_translation.h
@@ -0,0 +1,10 @@
+#include <torch/extension.h>
+
+#ifndef FUME_IMAGE_TRANSLATION_H
+#define FUME_IMAGE_TRANSLATION_H
+
+#endif //FUME_IMAGE_TRANSLATION_H
+
+torch::Tensor translate_image(
+        const torch::Tensor& input,
+        const torch::Tensor& F);
--- cuda/main.cpp
+++ cuda/main.cpp
@@ -1,7 +1,16 @@
 #include <torch/torch.h>
 #include <iostream>
+#include "image_translation.h"
 
 int main() {
-    torch::Tensor tensor = torch::rand({2, 3});
-    std::cout << tensor << std::endl;
+    auto options = torch::dtype(torch::kFloat32).device(torch::kCUDA).requires_grad(false);
+    torch::Tensor F = torch::tensor({{{  0.003976,   -0.944784,     469.808846},
+                                                        { -0.950176,   -0.000314,   -3336.818453},
+                                                        {462.822527, 4266.546518, -433140.893415}}}, options);
+
+    torch::Tensor view1 = torch::zeros({1, 1, 976, 976}, options);
+    torch::Tensor view2 = translate_image(view1, F);
+
+    std::cout << F << std::endl;
+    std::cout << view2 << std::endl;
 }
