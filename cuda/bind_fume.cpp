49a11b5aa701c94a9df608faa9756a0efba6355b
Maximilian Rohleder <Maxi.Rohleder@fau.de>
2022-11-03 14:56:16 +0100

    Removed unneccessary content
    * this branch only tracks assets like images etc.

--- cuda/bind_fume.cpp
+++
@@ -1,9 +0,0 @@
-//
-// This file is only needed to bind the cpp project to a python library with setuptools. See setup.py.
-//
-#include "image_translation.h"
-
-
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-m.def("translate", &translate_image, "Epipolar Image Translation (CUDA)");
-}
