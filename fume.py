Important Gradient Fix
* the backward method used to return only 3 parameters
* since adding the downsampled factor, another non-trainable factor must be returned
* added a None return

fume.py
@@ -42,7 +42,7 @@ class FumeImageTranslation(torch.autograd.Function):
         '''
         Finv = ctx.saved_tensors[0]
         downsampled_factor = ctx.saved_tensors[1]
-        return fume_torch_lib.translate(grad, Finv, downsampled_factor), None, None
+        return fume_torch_lib.translate(grad, Finv, downsampled_factor), None, None, None
 
 
 class Fume3dLayer(nn.Module):